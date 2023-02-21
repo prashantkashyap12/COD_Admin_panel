c0de1c5b64303ee6e79ad2c6271e2637c075f0a9
Prashant kashyap <Prashantkashyap12@gmail.com>
2023-02-21 23:08:49 +0530

    UI data update

--- src/app/panel-live-gammer/panel-live-gammer.component.ts
+++ src/app/panel-live-gammer/panel-live-gammer.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./panel-live-gammer.component.css']
 })
 export class PanelLiveGammerComponent {
+  liveGamer = [
+    { date:'01/10/2010', time:'18:12', name:'Prashant kashyap', contact:'886509012', market:'Delhi', batNo:'12', amount:'24', status:'live' },
+    { date:'01/10/2010', time:'18:12', name:'Prashant kashyap', contact:'886509012', market:'Delhi', batNo:'12', amount:'24', status:'live' },
+    { date:'01/10/2010', time:'18:12', name:'Prashant kashyap', contact:'886509012', market:'Delhi', batNo:'12', amount:'24', status:'live' },
+    { date:'01/10/2010', time:'18:12', name:'Prashant kashyap', contact:'886509012', market:'Delhi', batNo:'12', amount:'24', status:'live' },
+    { date:'01/10/2010', time:'18:12', name:'Prashant kashyap', contact:'886509012', market:'Delhi', batNo:'12', amount:'24', status:'live' },
+  ]
+
+  delGamer(data:any){
+    confirm('Do you want to Delete this user "'+this.liveGamer[data].date+'" record')
+    {
+      this.liveGamer.splice(data,1);
+    }
+  }
+
 
 }
--- src/app/panel-live-user/panel-live-user.component.ts
+++ src/app/panel-live-user/panel-live-user.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-panel-live-user',
@@ -6,5 +7,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./panel-live-user.component.css']
 })
 export class PanelLiveUserComponent {
+
+  userAction:boolean=true;
+
+  @ViewChild ('dataProfile') userForm!:NgForm;
+
+  profileData = [
+    {id:'101', Name: 'Prashant Kashyap', contact:'8865080562', address:'Agra', password:'1234567890'},
+    {id:'102', Name: 'Shivani Kashyap', contact:'8865080562', address:'Agra', password:'1234567890'},
+    {id:'103', Name: 'Prashant Kashyap', contact:'8865080562', address:'Agra', password:'1234567890'},
+    {id:'104', Name: 'Shiani Kashyap', contact:'8865080562', address:'Agra', password:'1234567890'},
+    {id:'105', Name: 'Prashant Kashyap', contact:'8865080562', address:'Agra', password:'1234567890'}
+  ]
+
+  //Add User
+  Addupdate(profile:any){
+    console.log(profile);
+    this.profileData.push(profile);
+  }
+
+  // Edit User
+    userEdi(data:any){
+    this.userAction=false;
+    console.log(this.profileData[data]);
+    this.userForm.setValue({
+      id: this.profileData[data].id,
+      Name: this.profileData[data].Name,
+      contact: this.profileData[data].contact,
+      address: this.profileData[data].address,
+      password: this.profileData[data].password
+    })
+
+  }
+
+  // Delete USER
+  userDel(data:any){
+    confirm("Do You want to permanently Delete = " + this.profileData[data].Name)
+    {
+      console.log();
+
+    }
+    
+  }
+
+
 
 }
